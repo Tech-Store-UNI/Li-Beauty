97fcafee5da12bb89b5b49c8783420c6aec8f484
bruno4sandrade-tech <bruno4s.andrade@gmail.com>
2026-04-21 14:08:42 -0300

    Página de Portfolio atualizada

--- src/components/PortfolioContent/PortfolioContent.tsx
+++ src/components/PortfolioContent/PortfolioContent.tsx
@@ -3,24 +3,31 @@ import { Box, Typography } from "@mui/material";
 export const PortfolioContent = () => {
   return (
     <Box sx={{ padding: "40px" }}>
+      
+      {/* TÍTULO */}
       <Typography variant="h4">
-        LI BEAUTY - Transformando Beleza em Arte
+        <Box
+          component="span"
+          sx={{
+            color: "#e57373",
+            fontWeight: "normal",
+            textTransform: "uppercase"
+          }}
+        >
+          LI BEAUTY
+        </Box>{" "}
+        - Transformando Beleza em Arte
       </Typography>
 
-      <Typography>
-        <Typography
-  sx={{
-    maxWidth: "600px",
-    lineHeight: 1.6,
-  }}
->
-  Cada trabalho realizado representa mais do que uma mudança estética.
-  É uma verdadeira transformação de beleza, autoestima e confiança.
-  No nosso portfólio, você encontra resultados que valorizam a essência
-  de cada cliente, respeitando seus traços e destacando o que há de mais
-  bonito de forma natural e harmoniosa.
-</Typography>
+      {/* TEXTO EM 5 LINHAS */}
+      <Typography sx={{ mt: 2, textAlign: "justify", maxWidth: "600px" }}>
+        Cada trabalho realizado representa mais do que uma mudança estética. <br />
+        É uma verdadeira transformação de beleza, autoestima e confiança. <br />
+        No nosso portfólio, você encontra resultados que valorizam a essência de cada cliente,
+        respeitando seus traços e destacando o que há de mais bonito
+        de forma natural e harmoniosa.
       </Typography>
+
     </Box>
   );
 };
